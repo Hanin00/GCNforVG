9388f657aefe7b9c9b4c00592765390a5820adac
Hanin00 <79493511+Hanin00@users.noreply.github.com>
2022-05-09 17:50:27 +0900

    :sparkles: first commit :rabbit::carrot:

---
+++ ConfirmCluster.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sys
+sys.path.append(r'./api/visual_genome_python_driver')
+from visual_genome import api as vg
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from visual_genome import api as vg
+from PIL import Image as PIL_Image
+import requests
+
+
+testFile = open('./cluster10000.txt', 'r')  # 'r' read의 약자, 'rb' read binary 약자 (그림같은 이미지 파일 읽을때)
+readFile = testFile.readline()
+freObjList = (readFile.replace("'", '')).split(',')
+freObjList = list(map(int, freObjList))
+
+df = pd.DataFrame(freObjList,columns = ['cluster'])
+
+print(df)
+
+cluster12 = df[df['cluster']==13]
+print(cluster12.head(5))
+
+aList = list(cluster12.head(5).index.tolist())
+
+for image_id in aList:
+    regions = vg.get_region_descriptions_of_image(id=image_id)
+    # print("The %s region descriptions is: %s" %(image_id,regions[0].phrase))
+    # print("The %s region descriptions is: %s" %(image_id,regions[1].phrase))
+    # print("The %s region descriptions is: %s" %(image_id,regions[2].phrase))
+    # print("The %s region descriptions is: %s" %(image_id,regions[3].phrase))
+    # print("The %s region descriptions is: %s" %(image_id,regions[4].phrase))
---
+++ VGShow.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append(r'./api/visual_genome_python_driver')
+from visual_genome import api as vg
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from visual_genome import api as vg
+from PIL import Image as PIL_Image
+import requests
+
+try:
+    from StringIO import StringIO as ReadBytes
+except ImportError:
+    print("Using BytesIO, since we're in Python3")
+    #from io import StringIO #..
+    from io import BytesIO as ReadBytes
+
+image_id1 = 53
+image_id2 = 54
+image_id3 = 62
+image_id4 = 116
+image_id5 = 190
+image1 = vg.get_image_data(id=image_id1)
+image2 = vg.get_image_data(id=image_id2)
+image3 = vg.get_image_data(id=image_id3)
+image4 = vg.get_image_data(id=image_id4)
+image5 = vg.get_image_data(id=image_id5)
+
+#print("The url of the image is: %s" % image.url)
+
+#egions = vg.get_region_descriptions_of_image(id=image_id)
+
+fig = plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+
+def ImgShow(image) :
+    response = requests.get(image.url)
+    img = PIL_Image.open(ReadBytes(response.content))
+    plt.imshow(img)
+    plt.tick_params(labelbottom="Idx : {image}", labelleft=False)
+    plt.show()
+
+#visualize_regions(image, regions[:])   # description을 idx 8번까지만 표시함
+ImgShow(image1)   # description을 idx 8번까지만 표시함
+ImgShow(image2)   # description을 idx 8번까지만 표시함
+ImgShow(image3)   # description을 idx 8번까지만 표시함
+ImgShow(image4)   # description을 idx 8번까지만 표시함
+ImgShow(image5)   # description을 idx 8번까지만 표시함
+
