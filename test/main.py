df82b01b0c0f6d1b0495c2a4cf54d1642d0eff5e
Hanin00 <hhaeun5419@gmail.com>
2022-05-19 08:10:55 +0000

    :construction:

---
+++ test/main.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import pickle
+import matplotlib.pyplot as plt
+
+
+
+with open('./data/adjMatrix1000.pickle.json') as file:  # open json file
+    data1 = json.load(file)
+with open('./data/featMatrix1000.pickle.json') as file:  # open json file
+    data1 = json.load(file)
+
+
+
+with open('./data/adjMatrix1000.pickle.json') as file:  # open json file
+    data1 = json.load(file)
+with open('./data/featMatrix1000.pickle.json') as file:  # open json file
+    data1 = json.load(file)
+
+
+
+with open('./data/adjMatrix1000.pickle.json') as file:  # open json file
+    data1 = json.load(file)
+with open('./data/featMatrix1000.pickle.json') as file:  # open json file
+    data1 = json.load(file)
+
