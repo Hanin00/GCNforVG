ea4231d7e3c5c4650ab56b091f4562abd12e4d99
Hanin00 <haeun5419@a.ut.ac.kr>
2022-05-26 15:56:35 +0900

    :sparkles: mkEdgeListper1 :sunglasses:

--- GCN_GphClassify/util.py
+++ GCN_GphClassify/util.py
@@ -1,35 +1,36 @@
 import json
 import pickle
 
+
 # scene_graph.json -> adjMatrix/FeatureMatrix 만들 때 오류나서 제외한 사진들이 있음
 # 해당 이미지를 제외하고 label 값을 추출해야해서 따로 처리함
-def siftCluster() :
+def siftCluster():
     testFile = open('./data/cluster5000.txt', 'r')
     readFile = testFile.readline()
     oldLabel = (readFile[1:-1].replace("'", '')).split(',')
     label = []
 
-    siftListId = [50,60, 156, 241, 284, 299, 317, 371, 403, 432, 512, 520, 647, 677, 745, 867, 930, 931, 1102, 1116, 1136, 1174, 1212, 1239, 1256, 1278]
+    siftListId = [50, 60, 156, 241, 284, 299, 317, 371, 403, 432, 512, 520, 647, 677, 745, 867, 930, 931, 1102, 1116,
+                  1136, 1174, 1212, 1239, 1256, 1278]
 
     for i in range(len(oldLabel)):
-        if i in siftListId :
+        if i in siftListId:
             continue
-        else :
+        else:
             label.append(i)
-        if(len(label) == 1000) :
+        if (len(label) == 1000):
             break
 
     print(len(label))
     with open("./data/clusterSifted1000.pickle", "wb") as fw:
         pickle.dump(label, fw)
 
-#siftCluster()
+
+# siftCluster()
 
 
 # netx에서 adjMatrix sparse 형태로 뽑아서 edge list로 사용하기
 def tupleToList():
-
-
     return
 
 
@@ -55,7 +56,31 @@ def tupleToList():
     data.test_neg_edge_index / test_pos_edge_index    
     data.train_neg_edge_index / train_pos_edge_index    
 '''
-def makeDataset() :
-    data
-    data.y =
+import time
+import networkx as nx
+import numpy as np
+import sys
+import pandas as pd
 
+with open("./data/networkx1000.pickle", "rb") as fr:
+    netx = pickle.load(fr)
+
+def mkEdgelistPer1(netx):
+    a = netx.edges.data()
+    sr = pd.DataFrame(a)
+    sr.columns = ['n1', 'n2', 'feature']
+    n1 = sr['n1'].values.tolist()
+    n2 = sr['n2'].values.tolist()
+    return n1, n2
+
+n1, n2 = mkEdgelistPer1(netx[0])  #local 저장?
+print(n1, n2)
+
+
+def mkEdgelistPer1(netx):
+    a = netx.edges.data()
+    sr = pd.DataFrame(a)
+    sr.columns = ['n1', 'n2', 'feature']
+    n1 = sr['n1'].values.tolist()
+    n2 = sr['n2'].values.tolist()
+    return n1, n2
